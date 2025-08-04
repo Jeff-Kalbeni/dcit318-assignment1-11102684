45949944c692860caf9a1f571f4a70922eeff24e
USER <jeffkalbeni@gmail.com>
2025-08-04 21:01:09 +0000

    First application that displays a menu to allow a user to choose between the apllications to use

---
+++ AssignmentOne/AssignmentOne/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using GradeCalculator;
+using TicketPriceCalculator;
+using TriangleTypeIdentifier;
+
+
+namespace AssignmentOne
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\tWelcome to my First Console App");
+            Console.WriteLine("1. Grade Calculator \n2. Ticket Price Calculator \n3. Triangle Type Identifier");
+            Console.Write("\nEnter your choice: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice) {
+                case 1:
+                    GradeCalculator.Program.CalculateGrade();
+                    break;
+                case 2:
+                    TicketPriceCalculator.Program.CalculateTicketPrice();
+                    break;
+                case 3:
+                    TriangleTypeIdentifier.Program.IdentifyTriangle();
+                    break;
+            }
+        }
+    }
+}
