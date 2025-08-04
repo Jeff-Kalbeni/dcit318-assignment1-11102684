befb9109373555ff77020daad84f313bbced4368
USER <jeffkalbeni@gmail.com>
2025-08-04 21:15:31 +0000

    This is the application to calculate the price of a movie theater ticket

---
+++ AssignmentOne/TicketPriceCalculator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketPriceCalculator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateTicketPrice();
+        }
+        public static void CalculateTicketPrice()
+        {
+            try
+            {
+                Console.Write("Welcome to The Movie Theater Ticket App \nEnter your age: ");
+
+                int age = Convert.ToInt32(Console.ReadLine());
+                if (age <= 12 || age >= 65)
+                {
+                    Console.WriteLine("Your ticket costs GHC 7.");
+                }
+                else
+                {
+                    Console.WriteLine("Your ticket costs GHC 10");
+                }
+            }
+            catch (Exception )
+            {
+
+                Console.WriteLine("Please enter a number");
+            }
+        }
+    }
+}
